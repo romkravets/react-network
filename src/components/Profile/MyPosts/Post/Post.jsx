6e2fdbc5806ebe6cf0e66f2b0f9b5a603c54d68c
romkravets <romann.kravets@gmail.com>
2020-06-28 15:06:32 +0300

    Update Post.jsx

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -6,6 +6,7 @@ const Post = () => {
         <div className={classes.item}>
         <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" />
           post 1
+          <span>like</span>
         </div>
    )
 }
