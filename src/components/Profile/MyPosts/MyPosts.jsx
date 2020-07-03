2a40cac80ab34b4ea38bbca69a8874c8c704df26
romkravets <romann.kravets@gmail.com>
2020-07-03 13:26:23 +0300

    add

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Post from './Post/Post';
 import classes from './MyPosts.module.css'
 
-const MyPosts = () => {
+const MyPosts = (props) => {
    return (
       <div>
          My posts
